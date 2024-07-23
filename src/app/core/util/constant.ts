55bbe9e8342feeb9c84fb36d6abaefbdde326c95
dmitrytitow <dmitrypatseyeu@gmail.com>
2024-07-23 12:34:46 +0200

    --feature add supabase

---
+++ src/app/core/util/constant.ts
@@ -0,0 +1,4 @@
+export const SUPA_BASE_CREDENTIONAL = {
+  BASE_URL: 'https://onkcipwfrkveiovzjquu.supabase.co',
+  BASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ua2NpcHdmcmt2ZWlvdnpqcXV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA3OTAzNjEsImV4cCI6MjAzNjM2NjM2MX0.4tUaySOh7khUqCsniIZkYUjXTHXtE1L-XlovcBqNKZg'
+}
