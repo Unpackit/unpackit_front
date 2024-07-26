a27ef14e6042a48e8aec9ce12414c220d601bdd0
scyberlife <s.cyberlife@gmail.com>
2024-07-26 13:26:50 +0300

    fix

---
+++ src/app/core/components/verification/verification.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'verification',
+  standalone: true,
+  imports: [],
+  templateUrl: './verification.component.html',
+  styleUrl: './verification.component.scss'
+})
+export class VerificationComponent {
+
+}
--- src/app/core/pages/verification-page/verification-page.component.ts
+++ src/app/core/pages/verification-page/verification-page.component.ts
@@ -1,9 +1,12 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {VerificationComponent} from "../../components/verification/verification.component";
 
 @Component({
   selector: 'verification-page',
   standalone: true,
-  imports: [],
+  imports: [
+    VerificationComponent
+  ],
   templateUrl: './verification-page.component.html',
   styleUrl: './verification-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
